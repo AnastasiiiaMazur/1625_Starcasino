95d69e51024c2a450a69ea5d298f0ec7f317cd1a
AnastasiiiaMazur <nmazur.dev@gmail.com>
2025-08-06 13:42:09 +0100

    main activity layout done and working on start page

--- app/src/main/java/com/stcs/oon/MainActivity.kt
+++ app/src/main/java/com/stcs/oon/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.stcs.oon
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var pageTitle: TextView
+    private lateinit var backButton: ImageView
+    private lateinit var settingsButton: ImageView
+    private lateinit var newRouteButton: ImageView
+    private lateinit var savedRoutesButton: ImageView
+    private lateinit var homeButton: ImageView
+    private lateinit var statsButton: ImageView
+    private lateinit var manualRouteButton: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        pageTitle = findViewById(R.id.pageTitle)
+        backButton = findViewById(R.id.backButton)
+        settingsButton = findViewById(R.id.settingsButton)
+        newRouteButton = findViewById(R.id.newRouteButton)
+        savedRoutesButton = findViewById(R.id.savedRoutesButton)
+        homeButton = findViewById(R.id.homeButton)
+        statsButton = findViewById(R.id.statsButton)
+        manualRouteButton = findViewById(R.id.manualRouteButton)
+
+
     }
 }
