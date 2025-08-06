9f03cf7d03f60f64a97f469a92c1057440aef545
AnastasiiiaMazur <nmazur.dev@gmail.com>
2025-08-06 13:47:02 +0100

    start page layout done

--- app/src/main/java/com/stcs/oon/MainActivity.kt
+++ app/src/main/java/com/stcs/oon/MainActivity.kt
@@ -5,8 +5,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
