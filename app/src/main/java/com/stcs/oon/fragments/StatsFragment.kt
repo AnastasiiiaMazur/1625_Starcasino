a2d09e9011b48fea9021e2de08119df7df16a33f
Anastasiia Mazur <nmazur.dev@gmail.com>
2025-08-11 14:44:59 +0100

    stats fragment done and basic chart drawing

--- app/src/main/java/com/stcs/oon/fragments/StatsFragment.kt
+++ app/src/main/java/com/stcs/oon/fragments/StatsFragment.kt
@@ -1,13 +1,77 @@
 package com.stcs.oon.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import com.stcs.oon.R
 
 class StatsFragment: Fragment(R.layout.fragment_stats) {
 
+    lateinit var barChart: BarChart
+
+    // on below line we are creating
+    // a variable for bar data
+    lateinit var barData: BarData
+
+    // on below line we are creating a
+    // variable for bar data set
+    lateinit var barDataSet: BarDataSet
+
+    // on below line we are creating array list for bar data
+    lateinit var barEntriesList: ArrayList<BarEntry>
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // on below line we are initializing
+        // our variable with their ids.
+        barChart = view.findViewById(R.id.idBarChart)
+
+        // on below line we are calling get bar
+        // chart data to add data to our array list
+        getBarChartData()
+
+        // on below line we are initializing our bar data set
+        barDataSet = BarDataSet(barEntriesList, "Bar Chart Data")
+
+        // on below line we are initializing our bar data
+        barData = BarData(barDataSet)
+
+        // on below line we are setting data to our bar chart
+        barChart.data = barData
+
+        // on below line we are setting colors for our bar chart text
+        barDataSet.valueTextColor = Color.BLACK
+
+        // on below line we are setting color for our bar data set
+        barDataSet.setColor(resources.getColor(R.color.dark_red))
+
+        // on below line we are setting text size
+        barDataSet.valueTextSize = 16f
+
+        // on below line we are enabling description as false
+        barChart.description.isEnabled = false
+    }
+
+    private fun getBarChartData() {
+        barEntriesList = ArrayList()
+
+        // on below line we are adding data
+        // to our bar entries list
+        barEntriesList.add(BarEntry(1f, 1f))
+        barEntriesList.add(BarEntry(2f, 2f))
+        barEntriesList.add(BarEntry(3f, 3f))
+        barEntriesList.add(BarEntry(4f, 4f))
+        barEntriesList.add(BarEntry(5f, 5f))
+        barEntriesList.add(BarEntry(6f, 1f))
+        barEntriesList.add(BarEntry(7f, 2f))
+        barEntriesList.add(BarEntry(8f, 3f))
+        barEntriesList.add(BarEntry(9f, 4f))
+
     }
 }
