a613f2a8b88bd5edbb13e5ec39281410b90fdc4d
AnastasiiiaMazur <nmazur.dev@gmail.com>
2025-08-11 11:09:32 +0100

    .

---
+++ app/src/main/java/com/stcs/oon/fragments/extra/SavedDetailsFragment.kt
@@ -0,0 +1,13 @@
+package com.stcs.oon.fragments.extra
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.stcs.oon.R
+
+class SavedDetailsFragment: Fragment(R.layout.fradment_saved_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
